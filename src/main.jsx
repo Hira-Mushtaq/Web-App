6a9246ab4a5abf9ee55fffdba526a9ab8d6b3488
Hira Mushtaq <hiramushtaq062619mm@gmail.com>
2023-11-01 08:05:42 -0700

    signUp

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import App from './App.jsx'
+import './main.css'
+//import './PasswordLoginWithFirebase.css'
+import PasswordLoginWithFirebase from './PasswordLoginWithFirebase'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+   <PasswordLoginWithFirebase/>
+   
+    
+  </React.StrictMode>,
+)
